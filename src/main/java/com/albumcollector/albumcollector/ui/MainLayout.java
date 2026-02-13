8b56228648013192800e4792f07194510e4c59f3
JoniHurme <jonihurme@gmail.com>
2026-02-13 09:42:04 +0200

    Added navbar. It yet has no functionality. Also added some comments and tested the get the amount of records shown.

--- src/main/java/com/albumcollector/albumcollector/ui/MainLayout.java
+++ src/main/java/com/albumcollector/albumcollector/ui/MainLayout.java
@@ -4,6 +4,7 @@ import com.albumcollector.albumcollector.model.entity.Record;
 import com.albumcollector.albumcollector.service.RecordService;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -15,17 +16,43 @@ public class MainLayout extends VerticalLayout {
     private final Grid<Record> grid;
     private final RecordService recordService;
 
-    MainLayout( RecordService recordService) {
+    MainLayout(RecordService recordService) {
+//      Init the nav bar and render it.
+        MenuBar navBar = buildNav();
+        add(navBar);
 
+
+        add(new H1("Album Collector"));
+//      End of Header. There will be an actual header sometime I hope.
+
+//      Init service and grid
         this.grid = new Grid<>(Record.class);
         this.recordService = recordService;
 
-        add(new H1("Album Collector"));
+//      Add grid component and some test functions.
+        add(String.valueOf(ammountOfRecords()));
         add(grid);
         listRecords();
+
     }
+
+//  Functions
     private void listRecords() {
         grid.setItems(recordService.findAll());
+    }
+    private long ammountOfRecords() {
+        long count = recordService.Count();
+        return count;
+    }
+//  The menu is configured.
+    private MenuBar buildNav() {
+        MenuBar navBar = new MenuBar();
+
+        navBar.addItem("Home");
+        navBar.addItem("Collection");
+        navBar.addItem("Wishlist");
+
+        return navBar;
     }
 
 }
