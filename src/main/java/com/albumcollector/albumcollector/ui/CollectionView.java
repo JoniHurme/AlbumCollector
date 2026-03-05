e534cde3b58a76055be1004ed356deaa522a1fb7
JoniHurme <jonihurme@gmail.com>
2026-03-05 10:26:55 +0200

    Added a card type of functionality to view details of a specific record.

--- src/main/java/com/albumcollector/albumcollector/ui/CollectionView.java
+++ src/main/java/com/albumcollector/albumcollector/ui/CollectionView.java
@@ -1,9 +1,15 @@
 package com.albumcollector.albumcollector.ui;
 import com.albumcollector.albumcollector.model.entity.Record;
 import com.albumcollector.albumcollector.service.RecordService;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Route;
 import java.util.List;
 
@@ -12,11 +18,15 @@ import java.util.List;
 public class CollectionView extends VerticalLayout {
         Grid<Record> recordGrid = new Grid<>(Record.class, false);
         private RecordService recordService;
+        private final Binder<Record> recordBinder = new Binder<>(Record.class);
+        private final Dialog dialog = new Dialog();
 
 
         public CollectionView(RecordService recordService){
         add(new H1("This is the collection page!"));
 
+//      Configuration must be called somewhere
+        ConfigureRecordDialog();
 
         recordGrid.addColumn(Record::getTitle).setHeader("Record title");
         recordGrid.addColumn(Record::getArtist).setHeader("Record artist");
@@ -26,7 +36,40 @@ public class CollectionView extends VerticalLayout {
         List<Record> recordList = recordService.findAll();
         recordGrid.setItems(recordList);
 
+        recordGrid.addItemClickListener(recordItemClickEvent -> {
+                openRecord(recordItemClickEvent.getItem());
+        });
+
 
         add(recordGrid);
     }
+
+//    Function that opens the recordView and binds the values from the clicked record.
+    private void openRecord(Record record) {
+        recordBinder.readBean(record);
+        dialog.open();
+    }
+//  configuring the dialog that the openRecord() function opens.
+    private void ConfigureRecordDialog() {
+            FormLayout recordForm = new FormLayout();
+
+            TextField title = new TextField("Title");
+            TextField artist = new TextField("Artists");
+            TextField genre = new TextField("Genre");
+            TextField medium = new TextField("Medium");
+            IntegerField year = new IntegerField("Year");
+            Checkbox favourite = new Checkbox("Favourite");
+
+            recordForm.add(title, artist, genre, medium, year, favourite);
+
+            recordBinder.forField(title).bind(Record::getArtist, null);
+            recordBinder.forField(artist).bind(Record::getTitle, null);
+            recordBinder.forField(genre).bind(Record::getGenre, null);
+            recordBinder.forField(medium).bind(Record::getMedium, null);
+            recordBinder.forField(year).bind(Record::getYear, null);
+            recordBinder.forField(favourite).bind(Record::getFavourite, null);
+
+            dialog.add(recordForm);
+    }
+
 }
