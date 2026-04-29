c08ac90eda0557841c4efff441076815856b1a16
joni <jonihurme@protonmail.com>
2026-04-29 15:24:26 +0300

    Updated the readme to include usage and deployment instructions and updated troubleshooting.

--- src/main/java/com/albumcollector/albumcollector/controller/RecordController.java
+++ src/main/java/com/albumcollector/albumcollector/controller/RecordController.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/records")
 @CrossOrigin(origins = "http://localhost:5173") // For Vite dev server
+//@CrossOrigin(origins = "http:/localhost:8085")
 public class RecordController {
 
     private final RecordService service;
